87f8c0e0ab68e5245c5255ea19e265f33bfc4b96
soseuleaf <peulri1379@gmail.com>
2023-06-01 18:30:28 +0900

    Fix: Weapon Balance
    * 스나이퍼 데미지 30% 하향
    * 대포 범위 너프

--- Assets/Scripts/Network/NetCannonProjectile.cs
+++ Assets/Scripts/Network/NetCannonProjectile.cs
@@ -19,7 +19,7 @@ namespace Network
             if(IsHit || !HasStateAuthority) return;
             var objectLayer = collision.collider.gameObject.layer;
 
-            if (objectLayer.Equals(LayerMask.NameToLayer("World")) || objectLayer.Equals(LayerMask.NameToLayer("Enemy")))
+            if (objectLayer.Equals(LayerMask.NameToLayer("World")))
             {
                 var hit = collision.contacts[0];
                 var point = hit.point - hit.normal * 0.01f;
@@ -35,7 +35,28 @@ namespace Network
                 Collider[] hitColliders = Physics.OverlapSphere(gameObject.transform.position, special, (int)Layer.Enemy);
                 if (hitColliders.Length > 0)
                 {
-                    GameManager.Instance.NetworkManager.AddCharacterHitData(Object, (int)Damage, _projectileHolder.WeaponData.isMainWeapon);
+                    GameManager.Instance.NetworkManager.AddCharacterHitData(Object, (int)DamageSave, _projectileHolder.WeaponData.isMainWeapon);
+                }
+
+                IsHit = true;
+            }
+            else if (objectLayer.Equals(LayerMask.NameToLayer("Enemy")))
+            {
+                var hit = collision.contacts[0];
+                var point = hit.point - hit.normal * 0.01f;
+                var special = (int)_baseStat(WeaponStat.Special).Total;
+
+                point.x = (float)Math.Round(point.x, 3);
+                point.y = (float)Math.Round(point.y, 3);
+                point.z = (float)Math.Round(point.z, 3);
+
+                WorldManager.Instance.GetWorld().ExplodeBlocks(point, special, (int)DamageSave);
+                GameManager.Instance.NetworkManager.AddBlockHitData(point, special, (int)DamageSave);
+                
+                Collider[] hitColliders = Physics.OverlapSphere(gameObject.transform.position, special, (int)Layer.Enemy);
+                if (hitColliders.Length > 0)
+                {
+                    GameManager.Instance.NetworkManager.AddCharacterHitData(Object, (int)DamageSave * 2, _projectileHolder.WeaponData.isMainWeapon);
                 }
 
                 IsHit = true;
